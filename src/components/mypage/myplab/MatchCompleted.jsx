96e4553e0e4fcea610f1ed0b16c448c8b3db18b9
김정섭 <jaydenkim850802@gmail.com>
2025-01-02 15:16:30 +0900

    mypage/mylevel 레벨 별 이미지 로드
    mypage/mylevel 레벨 별 이미지 로드

--- src/components/mypage/myplab/MatchCompleted.jsx
+++ src/components/mypage/myplab/MatchCompleted.jsx
@@ -19,7 +19,7 @@ const MatchCompleted = ({ selectedDate }) => {
     const fetchCompletedMatches = async () => {
       try {
         const response = await fetch(
-          "http://127.0.0.1:8080/mypage/myplabcomplete",
+          "http://127.0.0.1:8080/mypage/myplabcompleted",
           {
             method: "POST",
             headers: {
